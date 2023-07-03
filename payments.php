00fef4a175de60b435384a09b982a6918ca66e50
Padgett26 <padgett.jason@gmail.com>
2023-07-03 15:23:28 -0500

    Tweeks

--- payments.php
+++ payments.php
@@ -2,9 +2,14 @@
 <span style='font-weight: bold;'>Thank you</span>
 <br />
 <br />
-We hope our family of websites are helpful. And through your
-contribution to our sites we can continue to keep them free for those
-that need them.<br><br>
+We hope our family of websites are helpful. And through your contribution to our sites we can continue to keep them free for those that need them.<br><br>
+<div style='padding:20px; border:1px solid black; margin:20px; font-weight:bold;'>
+<span style='font-size:1.25em;'>Contribute through Venmo</span><br><br>
+@mylocallife<br><br>
+<img src='images/venmoScan.png' alt='@mylocallife' style='max-height:400px; margin:0px auto;'>
+</div>
+<div style='padding:20px; border:1px solid black; margin:20px; font-weight:bold;'>
+<span style='font-size:1.25em;'>Contribute through PayPal</span><br><br>
 <form action="https://www.paypal.com/donate" method="post" target="_top">
 	<input type="hidden" name="hosted_button_id" value="ARFLYDEMWR9QC" /> <input
 		type="image"
@@ -15,4 +20,5 @@ that need them.<br><br>
 		src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1"
 		height="1" />
 </form>
+</div>
 </div>
